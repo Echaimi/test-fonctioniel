0293e88060e44f3fcee669e62f33146a8b67db7c
Echaimi <echaimi@myges.fr>
2023-11-06 18:06:16 +0100

    Refacto evaluerLancer for clarity and maintainability

--- diceGame.js
+++ diceGame.js
@@ -1,4 +1,30 @@
-// diceGame.js
+const YAMS_SCORE = 50;
+const CARRE_SCORE = 35;
+const FULL_SCORE = 30;
+const BRELAN_SCORE = 28;
+const GRANDE_SUITE_SCORE = 40;
+
+function isYams(frequencies) {
+  return frequencies[0] === 5;
+}
+
+function isCarre(frequencies) {
+  return frequencies[0] === 4;
+}
+
+function isFull(frequencies) {
+  return frequencies[0] === 3 && frequencies[1] === 2;
+}
+
+function isBrelan(frequencies) {
+  return frequencies[0] === 3;
+}
+
+function isGrandeSuite(uniqueValues) {
+  if (uniqueValues.size !== 5) return false;
+  const sortedValues = [...uniqueValues].sort((a, b) => a - b);
+  return sortedValues.every((val, i, arr) => i === 0 || val - arr[i - 1] === 1);
+}
 
 function evaluerLancer(dés) {
   if (dés.length !== 5) {
@@ -10,19 +36,14 @@ function evaluerLancer(dés) {
     return acc;
   }, {});
 
-  const valeursUniques = new Set(dés);
   const frequences = Object.values(frequenceDes).sort((a, b) => b - a);
+  const valeursUniques = new Set(dés);
 
-  if (frequences[0] === 5) return 50; // YAMS
-  if (frequences[0] === 4) return 35; // Carré
-  if (frequences[0] === 3 && frequences[1] === 2) return 30; // Full
-  if (frequences[0] === 3) return 28; // Brelan
-
-  // Grande suite: vérifier si tous les dés se suivent
-  const estGrandeSuite = [...valeursUniques].sort((a, b) => a - b)
-    .every((val, i, arr) => i === 0 || val - arr[i - 1] === 1);
-  
-  if (estGrandeSuite && valeursUniques.size === 5) return 40; // Grande suite
+  if (isYams(frequences)) return YAMS_SCORE;
+  if (isCarre(frequences)) return CARRE_SCORE;
+  if (isFull(frequences)) return FULL_SCORE;
+  if (isBrelan(frequences)) return BRELAN_SCORE;
+  if (isGrandeSuite(valeursUniques)) return GRANDE_SUITE_SCORE;
 
   // Chance: aucune figure, retourner la somme des dés
   return dés.reduce((acc, val) => acc + val, 0);
