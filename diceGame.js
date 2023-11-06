b992e6284136de94dea1f4103a0447c6d79f3c3c
Echaimi <echaimi@myges.fr>
2023-11-06 19:17:31 +0100

    refacto multi lancé

--- diceGame.js
+++ diceGame.js
@@ -1,36 +1,39 @@
 const YAMS_SCORE = 50;
-const CARRE_SCORE = 35;
-const FULL_SCORE = 30;
-const BRELAN_SCORE = 28;
+const CARRE_SCORE = 35; 
+const FULL_SCORE = 30; 
+const BRELAN_SCORE = 28; 
 const GRANDE_SUITE_SCORE = 40;
 
-function isYams(frequencies) {
-  return frequencies[0] === 5;
+function areAllFiguresSelected(figuresSelectionnees) {
+  return Object.values(figuresSelectionnees).every(val => val);
 }
 
-function isCarre(frequencies) {
-  return frequencies[0] === 4;
+function isYams(frequences) {
+  return frequences.includes(5);
 }
 
-function isFull(frequencies) {
-  return frequencies[0] === 3 && frequencies[1] === 2;
+function isCarre(frequences) {
+  return frequences.includes(4);
 }
 
-function isBrelan(frequencies) {
-  return frequencies[0] === 3;
+function isFull(frequences) {
+  return frequences.includes(3) && frequences.includes(2);
 }
 
-function isGrandeSuite(uniqueValues) {
-  if (uniqueValues.size !== 5) return false;
-  const sortedValues = [...uniqueValues].sort((a, b) => a - b);
-  return sortedValues.every((val, i, arr) => i === 0 || val - arr[i - 1] === 1);
+function isBrelan(frequences) {
+  return frequences.includes(3) && !frequences.includes(2);
 }
 
-function evaluerLancer(dés) {
+function isGrandeSuite(valeursUniques) {
+  const sortedValues = [...valeursUniques].sort((a, b) => a - b);
+  return sortedValues.length === 5 && sortedValues[4] - sortedValues[0] === 4;
+}
+
+function evaluerLancer(dés, figuresSelectionnees) {
   if (dés.length !== 5) {
     throw new Error('Un lancer doit contenir exactement 5 dés.');
   }
-
+  
   const frequenceDes = dés.reduce((acc, val) => {
     acc[val] = (acc[val] || 0) + 1;
     return acc;
@@ -39,14 +42,56 @@ function evaluerLancer(dés) {
   const frequences = Object.values(frequenceDes).sort((a, b) => b - a);
   const valeursUniques = new Set(dés);
 
-  if (isYams(frequences)) return YAMS_SCORE;
-  if (isCarre(frequences)) return CARRE_SCORE;
-  if (isFull(frequences)) return FULL_SCORE;
-  if (isBrelan(frequences)) return BRELAN_SCORE;
-  if (isGrandeSuite(valeursUniques)) return GRANDE_SUITE_SCORE;
+  let score = dés.reduce((acc, val) => acc + val, 0);
+  let figure = 'CHANCE';
 
-  // Chance: aucune figure, retourner la somme des dés
-  return dés.reduce((acc, val) => acc + val, 0);
+  if (isYams(frequences) && !figuresSelectionnees.YAMS) {
+    score = YAMS_SCORE;
+    figure = 'YAMS';
+  } else if (isCarre(frequences) && !figuresSelectionnees.CARRE) {
+    score = CARRE_SCORE;
+    figure = 'CARRE';
+  } else if (isFull(frequences) && !figuresSelectionnees.FULL) {
+    score = FULL_SCORE;
+    figure = 'FULL';
+  } else if (isBrelan(frequences) && !figuresSelectionnees.BRELAN) {
+    score = BRELAN_SCORE;
+    figure = 'BRELAN';
+  } else if (isGrandeSuite(valeursUniques) && !figuresSelectionnees.GRANDE_SUITE) {
+    score = GRANDE_SUITE_SCORE;
+    figure = 'GRANDE_SUITE';
+  } else if (!figuresSelectionnees.CHANCE) {
+    figure = 'CHANCE';
+  }
+  if (figure !== 'CHANCE' && figuresSelectionnees[figure]) {
+    return { score: 0, figure: null };
+  }
+  
+  if (figuresSelectionnees[figure]) {
+    return { score: 0, figure: null };
+  } else {
+    figuresSelectionnees[figure] = true;
+    return { score, figure };
+  }
 }
 
-module.exports = { evaluerLancer };
+function evaluerPlusieursLancers(lancers) {
+  const figuresSelectionnees = {
+    YAMS: false,
+    CARRE: false,
+    FULL: false,
+    BRELAN: false,
+    GRANDE_SUITE: false,
+    CHANCE: false
+  };
+
+  return lancers.map(lancer => {
+    const resultat = evaluerLancer(lancer, figuresSelectionnees);
+    if (resultat.figure !== null) {
+      figuresSelectionnees[resultat.figure] = true;
+    }
+    return resultat;
+  });
+}
+
+module.exports = { evaluerLancer, evaluerPlusieursLancers };
--- diceGame.test.js
+++ diceGame.test.js
@@ -1,31 +1,92 @@
-const { evaluerLancer } = require('./diceGame');
+const { evaluerLancer,evaluerPlusieursLancers} = require('./diceGame');
 
 describe('Tests pour evaluerLancer', () => {
-  test('YAMS avec 5 dés identiques', () => {
-    expect(evaluerLancer([1, 1, 1, 1, 1])).toBe(50);
+    test('YAMS avec 5 dés identiques', () => {
+      expect(evaluerLancer([1, 1, 1, 1, 1], {}).score).toBe(50);
+    });
+  
+    test("Carré avec 4 dés identiques", () => {
+      expect(evaluerLancer([2, 2, 2, 2, 5], {}).score).toBe(35);
+    });
+  
+    test('Full avec 3 dés identiques et une paire', () => {
+      expect(evaluerLancer([4, 4, 4, 1, 1], {}).score).toBe(30);
+    });
+  
+    test('Brelan avec 3 dés identiques', () => {
+      expect(evaluerLancer([3, 3, 3, 4, 5], {}).score).toBe(28);
+    });
+  
+    test('Grande suite avec 5 dés se suivant', () => {
+      expect(evaluerLancer([2, 3, 4, 5, 6], {}).score).toBe(40);
+    });
+  
+    test('0 points quand toutes les figures sont déjà sélectionnées', () => {
+      const figuresSelectionnees = {
+        "YAMS": true,
+        "CARRE": true,
+        "FULL": true,
+        "BRELAN": true,
+        "PETITE_SUITE": true,
+        "GRANDE_SUITE": true,
+        "CHANCE": true,
+      };
+      const result = evaluerLancer([2, 2, 2, 5, 6], figuresSelectionnees);
+      expect(result.score).toBe(0);
+      expect(result.figure).toBeNull(); // or toBe(undefined) depending on the implementation
+    });
+  
+    test('Chance avec des dés désordonnés ne formant aucune figure', () => {
+      expect(evaluerLancer([2, 3, 4, 6, 1], {}).score).toBe(16);
+    });
+  });
+  
+describe('Tests pour evaluerPlusieursLancers', () => {
+  test('évalue plusieurs lancés avec des figures différentes', () => {
+    const lancers = [
+      [1, 1, 1, 2, 2], // FULL - 30 points
+      [2, 2, 2, 3, 3], // FULL déjà pris, devrait choisir BRELAN - 28 points
+      [4, 4, 4, 4, 5], // CARRE - 35 points
+      [1, 2, 3, 4, 5], // GRANDE_SUITE - 40 points
+      [1, 1, 1, 1, 1], // YAMS - 50 points
+      [2, 3, 4, 5, 6]  // CHANCE, car les autres figures sont prises - somme des dés = 20 points
+    ];
+
+    const expectedResults = [
+      { score: 30, figure: 'FULL' },
+      { score: 28, figure: 'BRELAN' },
+      { score: 35, figure: 'CARRE' },
+      { score: 40, figure: 'GRANDE_SUITE' },
+      { score: 50, figure: 'YAMS' },
+      { score: 20, figure: 'CHANCE' }
+    ];
+    const results = evaluerPlusieursLancers(lancers);
+    expect(results).toEqual(expectedResults);
   });
 
-  test('Carré avec 4 dés identiques', () => {
-    expect(evaluerLancer([2, 2, 2, 2, 5])).toBe(35);
-  });
+  test('retourne 0 pour tous les lancés après que toutes les figures ont été prises', () => {
+    const lancers = [
+      [1, 1, 1, 1, 1], // YAMS - 50 points
+      [2, 2, 2, 2, 3], // CARRE - 35 points
+      [3, 3, 3, 4, 4], // FULL - 30 points
+      [4, 4, 4, 5, 6], // BRELAN - 28 points, assuming you can score a BRELAN if FULL is taken
+      [1, 2, 3, 4, 5], // GRANDE_SUITE - 40 points
+      [2, 3, 4, 5, 6], // CHANCE - somme des dés = 20 points
+      [1, 1, 1, 2, 3], // Tous pris, devrait être 0
+      [4, 4, 4, 4, 4]  // Tous pris, devrait être 0
+    ];
 
-  test('Full avec 3 dés identiques et une paire', () => {
-    expect(evaluerLancer([4, 4, 4, 1, 1])).toBe(30);
-  });
-
-  test('Brelan avec 3 dés identiques', () => {
-    expect(evaluerLancer([3, 3, 3, 4, 5])).toBe(28);
-  });
-
-  test('Grande suite avec 5 dés se suivant', () => {
-    expect(evaluerLancer([2, 3, 4, 5, 6])).toBe(40);
-  });
-
-  test('Chance avec des dés ne formant aucune figure', () => {
-    expect(evaluerLancer([1, 2, 3, 4, 6])).toBe(16);
-  });
-
-  test('Chance avec des dés désordonnés ne formant aucune figure', () => {
-    expect(evaluerLancer([2, 3, 4, 6, 1])).toBe(16);
+    const expectedResults = [
+      { score: 50, figure: 'YAMS' },
+      { score: 35, figure: 'CARRE' },
+      { score: 30, figure: 'FULL' },
+      { score: 28, figure: 'BRELAN' },
+      { score: 40, figure: 'GRANDE_SUITE' },
+      { score: 20, figure: 'CHANCE' },
+      { score: 0, figure: null },
+      { score: 0, figure: null }
+    ];
+    const results = evaluerPlusieursLancers(lancers);
+    expect(results).toEqual(expectedResults);
   });
 });
